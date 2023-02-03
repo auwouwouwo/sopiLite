e6d6f6b970dd30ba97b827de1e486e2b988a6a79
Mas Gendon <121809771+auwouwouwo@users.noreply.github.com>
2023-02-03 22:13:41 +0700

    Pindahin ke dalem src
    komponen css sama page

---
+++ depan/src/page/loginPage.js
@@ -0,0 +1,14 @@
+const Login = ()=>{
+    return(
+        <>
+            <div>
+                <form>
+                    <input type="text"/>
+                    <input type={password} />
+                    <input type={button} className="input-button-login" />
+                </form>
+            </div>
+        </>
+    )};
+
+export default Login;
