bfe220f64f968bffc889f404ec805c268965fa61
Mas Gendon <121809771+auwouwouwo@users.noreply.github.com>
2023-02-03 20:59:30 +0700

    Nambain server mongodb

---
+++ index.js
@@ -0,0 +1,13 @@
+const mongoose  = require("mongoose");
+const express   = require("express");
+const cors      = require("cors");
+
+const app       = express();
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://auwouwouwo:vOgqo9pxZsBgBsyT@cluster0.hznhnn3.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser     : true,
+    useUnifiedTopology  : true
+});
+
+app.listen(5000, ()=>console.log("Server terkoneksi di port 5000..."));
