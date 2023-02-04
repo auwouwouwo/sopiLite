c5cce2d6fbaa9a9a0d7ace0223ca11bef30998fd
Mas Gendon <121809771+auwouwouwo@users.noreply.github.com>
2023-02-04 19:40:58 +0700

    Add options server
    Coba-coba

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ const db        = mongoose.connection;
 
 const server    = async()=>{try{url()}catch(e){handleError(e)}};
 
-db.on("error", (e)=>logError(e));
-db.once("open", ()=>console.log("Database telah terkoneksi..."));
-
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
 server();
-
-app.listen(5000, ()=>console.log("Server terkoneksi di port 5000..."));
+db.on("error", (e)=>logError(e));
+db.once("open", ()=>console.log("Database telah terkoneksi..."));
+app.listen(5000, ()=>console.log("Server terkoneksi: Port 5000..."));
--- url/url.js
+++ url/url.js
@@ -1,8 +1,9 @@
 const mongoose  = require("mongoose");
+const options   = {family: 4, useUnifiedTopology: true};
 const url       = ()=>{
     mongoose.set("strictQuery", true);
-    u = mongoose.connect(`mongodb+srv://auwouwouwo:vOgqo9pxZsBgBsyT@cluster0.hznhnn3.mongodb.net/?retryWrites=true&w=majority`); 
-    return (u);
+    u = mongoose.connect(`mongodb+srv://auwouwouwo:vOgqo9pxZsBgBsyT@cluster0.hznhnn3.mongodb.net/?retryWrites=true&w=majority`,
+    options); 
 }
 
 module.exports  = url;
