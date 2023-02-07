699a3cf4c85d666d4c89f4aef4ae2fba603d1a58
FajarFE <fajarfernandi123123@gmail.com>
2023-02-07 17:44:52 +0700

    barusaja

--- depan/src/component/register.js
+++ depan/src/component/register.js
@@ -1,45 +1,44 @@
-
-const Register = ()=>{
-    const [HasAgree, setHasAgreed]=react.useState(false)
+import React from 'react'
+function Register(){
+    const [hasAgreed, setHasAgreed] = React.useState(true);
     return (
-    <>
-    <div className="text-form">
-    <h1 className="text-primary ">Register</h1>
-    <form action="">
-        <div className="form-group">
-            <label htmlFor="username" >Username</label>
-            <br/>
-            <input type="text" /**onClick={(e)=>setUsername(e.target.value)} **/placeholder='Masukan Username' />
+        <>
+        <div className="text-form">
+        <h1 className="text-primary ">Register</h1>
+        <form action="">
+            <div className="form-group">
+                <label htmlFor="username" >Username</label>
+                <br/>
+                <input type="text" /**onClick={(e)=>setUsername(e.target.value)} **/placeholder='Masukan Username' />
+            </div>
+            <div className="form-group">
+                <label htmlFor="gmail" >Gmail</label>
+                <br/>
+                <input type="text"  /**onClick={(e)=>setGmail(e.target.value)}**/ placeholder='Masukan Gmail' />  
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <br/>
+                <input type="text" /**onClick={(e)=>setPassword(e.target.value)}**/ placeholder='Masukan Password' /> 
+            </div>
+            <div className="form-group">
+                <label htmlFor="konfirmasi" >Konfirmasi</label>
+                <br/>
+                <input type="text" placeholder='Masukan Password' />
+                <br/>
+               
+            </div>
+            <div className="form-group checkbox">
+            <input type="checkbox" name="checkbox" id="checkbox" value={hasAgreed} onChange={()=>setHasAgreed(!hasAgreed)}/>      
+            <label htmlFor="checkbox">I Agree</label>
+            </div>
+            <div className="form-group">
+            <button style={{color:'white'}} className="btn btn-primary btn-daftar" disabled={hasAgreed} >DAFTAR</button>
+            </div>
+        </form> 
         </div>
-        <div className="form-group">
-            <label htmlFor="gmail" >Gmail</label>
-            <br/>
-            <input type="text"  /**onClick={(e)=>setGmail(e.target.value)}**/ placeholder='Masukan Gmail' />  
-        </div>
-        <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <br/>
-            <input type="text" /**onClick={(e)=>setPassword(e.target.value)}**/ placeholder='Masukan Password' /> 
-        </div>
-        <div className="form-group">
-            <label htmlFor="konfirmasi" >Konfirmasi</label>
-            <br/>
-            <input type="text" placeholder='Masukan Password' />
-            <br/>
-           
-        </div>
-        <div className="form-group checkbox">
-        <input type="checkbox" name="checkbox" id="checkbox" value={HasAgree} onChange={()=>setHasAgreed(!HasAgree)}/>      
-        <label htmlFor="checkbox">I Agree</label>
-        </div>
-        <div className="form-group">
-        <button style={{color:'white'}} className="btn btn-primary btn-daftar" isEn>DAFTAR</button>
-        </div>
-    </form> 
-    </div>
-    </>
+        </>
     )
-    
 }
 
 export default Register 
